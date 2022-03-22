cdc4c23f3f80bc977fe94bdbdbf28d576ea4de21
Dom Bryan <dombguitar@gmail.com>
2022-03-22 16:32:42 +0000

    Initial Commit

---
+++ LayoutAndGeometry/LayoutAndGeometryApp.swift
@@ -0,0 +1,17 @@
+//
+//  LayoutAndGeometryApp.swift
+//  LayoutAndGeometry
+//
+//  Created by Dom Bryan on 22/03/2022.
+//
+
+import SwiftUI
+
+@main
+struct LayoutAndGeometryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
