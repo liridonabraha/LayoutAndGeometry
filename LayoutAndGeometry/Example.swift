ab2ccea2ae84d3a8cc63d4e31a4fedb4fd58f125
Liridona <donabraha94@gmail.com>
2022-05-09 12:18:18 +0100

    Project 18

---
+++ LayoutAndGeometry/Example.swift
@@ -0,0 +1,20 @@
+//
+//  Example.swift
+//  LayoutAndGeometry
+//
+//  Created by Dom Bryan on 04/04/2022.
+//
+
+import SwiftUI
+
+struct Example: View {
+    var body: some View {
+        Text(" sdjsjdbshkdfbkfbkdbkjdbjdbfldbldbf jldvb jdv jls")
+    }
+}
+
+struct Example_Previews: PreviewProvider {
+    static var previews: some View {
+        Example()
+    }
+}
